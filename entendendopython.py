c903d332338787172dc409aaf3e8c6181a495268
Akihiikko <imjaecxy@gmail.com>
2026-05-12 17:39:05 -0300

    aprendendo operadores relacionais em python

---
+++ entendendopython.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+print("Isso é um texto")
+print ("1") #str (String de caracteres)
+print (1) #int (Numero inteiro)
+print (-2) #int (Numero inteiro)
+print (8.2) #float (Numero real ou decimal, ponto flutuante)
+print (False) # Verdadeiro ou
+print (True) # Falso (Tipo boolean -bool)
+
+nome = "Amanda"
+idade = 19
+peso = 62.6
+maiorIdade = True
+print ("##############################################################")
+print ("Essa é ", nome,", ela tem ", idade," e pesa ", peso,"kg")
+
+print ("O nome dela é " + nome +" eu encontrei ela no parque")
+
+print ("Hoje é o aniversário da {} e ela está completando {} anos e pesa {} kg, alem disso ela é maior de idade:".format (nome, idade, peso, maiorIdade))
+
+# // p0ega somente a parte inteira
+# / faz a divisão e mostra o valor quebrado 
---
+++ programa01.py
@@ -0,0 +1,32 @@
+n1 = int(input("Digite o 1 numero:"))
+n2 = int(input("Digite o 2 numero:"))
+
+
+resultado = n1 // n2 #  Divide e mostra o valor inteiro 
+resultado2 = n1 % n2 # Mostra o resto da divisão
+resultado3 = n1 ** n2 # Potencia
+
+print("resultado da parte inteira da divisão é :", resultado)
+print("resultado2 do resto da divisão é", resultado2)
+print("resultado3 da potencia é :", resultado3)
+
+#Opewradores Relacionais
+# == -  IGUAL
+# > maior
+# < menor
+# >= maior ou igual
+# <= menor ou igual
+# != não igual
+# Na programacao o sinal de = é para receber algo, já == é igual.
+print ("_________________________________________________")
+print ("  OPERADORES RELACIONAIS    ")
+print ("_________________________________________________")
+
+relacao1 = n1 > n2 # é boolean pois so retorna true or false
+relacao2 = n1 < n2
+relacao3 = n1 >= n2
+relacao4 = n1 <= n2
+relacao5 = n1 == n2
+relacao6 = n1 != n2
+
+print("Os resultados das relações estarão abaixo \n{} \n{} \n{} \n{} \n{} \n{}".format(relacao1, relacao2, relacao3, relacao4, relacao5, relacao6))
