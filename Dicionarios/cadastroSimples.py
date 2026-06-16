ecb5c02f97d1f3e1812013ddb645cc3a0dca05de
Akihiikko <imjaecxy@gmail.com>
2026-06-16 14:42:03 -0300

    Passando parametros. Funções!

---
+++ Dicionarios/cadastroSimples.py
@@ -0,0 +1,7 @@
+print("                       ✧˖°. ⋆｡˚✧˖°. ⋆｡˚(✿ᴗ͈ˬᴗ͈)｡･:*˚:✧˖°. ⋆｡˚✧｡\n")
+print("                       ૮꒰˶• ༝ •˶꒱ა  Cadastro Simples  (*ᴗ͈ˬᴗ͈)ꕤ*.ﾟ\n")
+print("                        ✧˖°. ⋆｡˚✧˖°. ⋆｡˚(✿ᴗ͈ˬᴗ͈)｡･:*˚:✧｡✧˖°. ⋆｡˚\n")
+
+nome, idade, cidade = input("಄ Digite seu nome, idade e cidade: ")
+
+
---
+++ funcao/funcaoDef.py
@@ -0,0 +1,11 @@
+#funcao simples n recebe parametro e so faz o que está ali
+#def acordar (): -- def é definir
+    # print("Bom Dia")
+#acordar()
+
+# ela precisa receber parametros
+# def somar(n1, n2):<- (parametros)
+#   soma = n1 + n2
+#   print (soma)
+# temos q passar os valores que queremos somar para não ser um parametro simples.
+#somar(2,5)
