a1bdcd3bce4f8c0b193ae3472dd3bd017fea2954
Akihiikko <imjaecxy@gmail.com>
2026-06-16 15:17:52 -0300

    Lista Mercadinhos

--- funcao/funcaoDef.py
+++ funcao/funcaoDef.py
@@ -1,11 +1,20 @@
-#funcao simples n recebe parametro e so faz o que está ali
+#funcao simples n recebe parametro e so faz o que está ali, sem retorno
 #def acordar (): -- def é definir
     # print("Bom Dia")
 #acordar()
 
 # ela precisa receber parametros
-# def somar(n1, n2):<- (parametros)
+# def somar(n1, n2): <- (parametros)
 #   soma = n1 + n2
 #   print (soma)
-# temos q passar os valores que queremos somar para não ser um parametro simples.
+# temos q passar os valores que queremos somar para não ser um parametro simples. sem retorno
 #somar(2,5)
+
+##################################################
+#def subtrair(n1, n2): <-- tem parametro e tem retorno
+#   return n1-n2
+#   subtrair(8, 3)
+
+#def mult(n1, n2, n3):
+#   mult = n1 * n2* n3
+#mult(5, 3, 7)
